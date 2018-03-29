da69bb12149b45de7d925e183fc444a6e82c08de
MariaJoseF <mjrf85@gmail.com>
2018-03-29 16:54:05 +0100

    Updates

---
+++ SpeechTest/WindowTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SpeechTest
+{
+    public partial class WindowTest : Gtk.Window
+    {
+        public WindowTest() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+        }
+    }
+}
